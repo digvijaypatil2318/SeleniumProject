b07ae70f5ae547b992e4e8d627cd8069f72a60b2
DELL <DELL@DESKTOP-A7RCVA4>
2021-11-06 16:30:22 +0530

    git new class added

---
+++ SeleniumProject/src/main/java/SeleniumAgain/GitRepository.java
@@ -0,0 +1,10 @@
+package SeleniumAgain;
+
+public class GitRepository {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SeleniumProject/src/main/java/SeleniumAgain/InterviewQuestion2.java
+++ SeleniumProject/src/main/java/SeleniumAgain/InterviewQuestion2.java
@@ -1,5 +1,5 @@
 package SeleniumAgain;
-
+ 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -21,15 +21,16 @@ public class InterviewQuestion2 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		WebElement wq = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-		wq.sendKeys("Mi11");
+		wq.sendKeys("samsung");
 		String m = wq.getAttribute("value");
 		System.out.println(m);
 
 		driver.findElement(By.id("nav-search-submit-button")).click();
 		driver.navigate().refresh();
-		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span")).click();
-		Thread.sleep(3000);
-		WebElement wd = driver.findElement(By.xpath("//input[@name='submit.add-to-cart']"));
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[4]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2")).click();
+		Thread.sleep(30000);
+		WebElement wd = driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]"));
 		String v = wd.getText();
 		System.out.println(v);
 		wd.click();
